e03a0343ee08b400b178867f11c4e2866ba6aaad
brennerhaverlock <brenner.haverlock@gmail.com>
2020-08-27 18:18:45 -0400

    added print
    added print functions

--- pyCalc.py
+++ pyCalc.py
@@ -18,3 +18,13 @@ def divide(x, y):
 
 def exp(x, y):
     return x ** y
+
+
+# user input
+
+print("Pick an operation:")
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+print("5. Raise a power")
