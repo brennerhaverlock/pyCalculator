2a5bb490540e38812280f8b1f6f3b95017ccfd51
brennerhaverlock <brenner.haverlock@gmail.com>
2020-08-27 15:35:42 -0400

    added operations
    operations added

--- pyCalc.py
+++ pyCalc.py
@@ -1,1 +1,20 @@
-def
+# added operations
+
+def add(x, y):
+    return x + y
+
+
+def subtract(x, y):
+    return x - y
+
+
+def multiply(x, y):
+    return x * y
+
+
+def divide(x, y):
+    return x / y
+
+
+def exp(x, y):
+    return x ** y
